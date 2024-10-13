f660d53181ba069d1b72bb427e8cbf39740bddc5
TECH-USER001 <junebkhan021@gmail.com>
2024-10-13 05:34:43 -0700

    Update simple intrest.js

--- simple intrest.js
+++ simple intrest.js
@@ -1,8 +1,8 @@
 // program for simple intrest
 let P = 10, R = 40, T = 15;
 
-/* Calculate simple interest */
+// Calculate simple interest 
 let SI = (P * T * R) / 100;
 
-/* Print the resultant value of SI */
+// Print the resultant value of SI 
 console.log("Simple Interest = " + SI);
