9db12909d444c493b197d1c89b71785c45e2c7f2
TECH-USER001 <junebkhan021@gmail.com>
2024-10-13 05:32:00 -0700

    Add files via upload

---
+++ simple intrest.js
@@ -0,0 +1,8 @@
+// program for simple intrest
+let P = 10, R = 40, T = 15;
+
+/* Calculate simple interest */
+let SI = (P * T * R) / 100;
+
+/* Print the resultant value of SI */
+console.log("Simple Interest = " + SI);
